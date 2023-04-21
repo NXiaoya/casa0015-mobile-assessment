6834889fe48db745ad6150ee29bb6ac39b4c4a13
Xiaoya <114834818+NXiaoya@users.noreply.github.com>
2023-04-21 17:12:58 +0100

    create the main screen and shift to drawer

--- pet_home/lib/pages/HomeScreen.dart
+++ pet_home/lib/pages/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pet_home/configuration.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,8 +7,49 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  double xOffset =0;
+  double yOffset =0;
+  double scaleFactor =1;
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return AnimatedContainer(
+      transform: Matrix4.translationValues(xOffset, yOffset, 0)..scale(scaleFactor),
+
+      duration: Duration(milliseconds: 250),
+      color: Colors.white,
+      child: Column(
+        children:[
+          SizedBox(height: 60,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(icon: Icon(Icons.menu), onPressed:(){
+                setState(() {
+                  xOffset =230;
+                  yOffset=150;
+                  scaleFactor=0.6;
+                });
+              }),
+              Column(
+                children: [
+                  Text('Location'),
+                  Row(
+                    children: [
+                      Icon(Icons.location_on,color: amber,),
+                      Text('London'),
+                    ],
+                  )
+                ],
+              ),
+              CircleAvatar()
+            ],
+          )
+        ]
+      ),
+    );
+
   }
 }
