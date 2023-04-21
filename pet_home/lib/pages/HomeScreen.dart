2583c8463c2bb46f9c5e99fc9c0b628f88990162
Xiaoya <114834818+NXiaoya@users.noreply.github.com>
2023-04-22 00:19:24 +0100

    create a detail screen

--- pet_home/lib/pages/HomeScreen.dart
+++ pet_home/lib/pages/HomeScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pet_home/pages/detailScreen.dart';
 import 'package:pet_home/configuration.dart';
-
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -18,108 +18,149 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
-        transform: Matrix4.translationValues(xOffset, yOffset, 0),
+      transform: Matrix4.translationValues(xOffset, yOffset, 0),
       duration: Duration(milliseconds: 250),
       decoration: BoxDecoration(
-          color: Colors.grey[200],
-
-          borderRadius: BorderRadius.circular(isDrawerOpen?40:0.0)
-
-      ),
-      child: Column(
-        children: [
-          SizedBox(height: 60,),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            child:
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      isDrawerOpen?IconButton(
-                        icon: Icon(Icons.arrow_back_ios),
-                        onPressed: (){
-                          setState(() {
-                            xOffset=0;
-                            yOffset=0;
-                            scaleFactor=1;
-                            isDrawerOpen=false;
-                          });
-                        },
-
-                      ): IconButton(
-                          icon: Icon(Icons.menu),
-                          onPressed: () {
-                            setState(() {
-                              xOffset = 230;
-                              yOffset = 150;
-                              scaleFactor = 0.6;
-                              isDrawerOpen=true;
-                            });
-                          }),
-                      Column(
+          color: Colors.yellow.shade100,
+          borderRadius: BorderRadius.circular(isDrawerOpen ? 40 : 0.0)),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 60,
+            ),
+            Container(
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    isDrawerOpen
+                        ? IconButton(
+                            icon: Icon(Icons.arrow_back_ios),
+                            onPressed: () {
+                              setState(() {
+                                xOffset = 0;
+                                yOffset = 0;
+                                scaleFactor = 1;
+                                isDrawerOpen = false;
+                              });
+                            },
+                          )
+                        : IconButton(
+                            icon: Icon(Icons.menu),
+                            onPressed: () {
+                              setState(() {
+                                xOffset = 230;
+                                yOffset = 150;
+                                scaleFactor = 0.6;
+                                isDrawerOpen = true;
+                              });
+                            }),
+                    Column(
+                      children: [
+                        Text('Location'),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              color: amber,
+                            ),
+                            Text('London'),
+                          ],
+                        )
+                      ],
+                    ),
+                    CircleAvatar()
+                  ],
+                )),
+            Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                margin: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                decoration: BoxDecoration(
+                    color: Colors.yellow.shade50,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.search),
+                    Text('Search pet to adopt'),
+                    Icon(Icons.settings)
+                  ],
+                )),
+            Container(
+              height: 120,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      child: Column(
                         children: [
-                          Text('Location'),
-                          Row(
-                            children: [
-                              Icon(Icons.location_on,color: amber,),
-                              Text('London'),
-                            ],
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            margin: EdgeInsets.only(left: 20),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                boxShadow: shadowList,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Image.asset(
+                              categories[index]['iconPath'],
+                              height: 40,
+                              width: 150,
+                              color: Colors.grey[700],
+                            ),
+                          ),
+                          Text(categories[index]['name'])
+                        ],
+                      ),
+                    );
+                  }),
+            ),
+            GestureDetector(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>detailScreen()));
+              },
+              child: Container(
+                height: 240,
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Stack(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.amber.shade100,
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: shadowList,
+                            ),
+                            margin: EdgeInsets.only(top: 40, bottom: 10),
+                          ),
+                          Align(
+                            child: Hero(
+                              tag: 1,
+                                child: Image.asset('images/petcat1.png')),
                           )
                         ],
                       ),
-                      CircleAvatar()
-                    ],
-                  )
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-            margin: EdgeInsets.symmetric(vertical: 15,horizontal: 10),
-            decoration: BoxDecoration(
-                color: Colors.yellow.shade50,
-                borderRadius: BorderRadius.circular(20)
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Icon(Icons.search),
-                Text('Search pet to adopt'),
-                Icon(Icons.settings)
-              ],
-             )
-          ),
-          Container(
-            height: 120,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: categories.length,
-                itemBuilder: (context,index){
-                  return Container(
-                    child: Column(
-                      children: [
-                        Container(
-
-                          padding: EdgeInsets.all(10),
-                          margin: EdgeInsets.only(left: 20),
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              boxShadow: shadowList,
-                              borderRadius: BorderRadius.circular(10)
-                          ),
-                          child: Image.asset(categories[index]['iconPath'],
-                            height: 40,
-                            width: 150,
-                            color: Colors.grey[700],
-                          ),
-                        ),
-                        Text(categories[index]['name'])
-                      ],
                     ),
-                  );
-                }
+                    Expanded(
+                        child: Container(
+                      margin: EdgeInsets.only(top: 60, bottom: 20),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: shadowList,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(20),
+                            bottomRight: Radius.circular(20),
+                          )),
+                    ))
+                  ],
+                ),
+              ),
             ),
-          ),
-          Container(
-            height: 240,
+            Container(
+              height: 240,
               margin: EdgeInsets.symmetric(horizontal: 20),
               child: Row(
                 children: [
@@ -127,33 +168,36 @@ class _HomeScreenState extends State<HomeScreen> {
                     child: Stack(
                       children: [
                         Container(
-                          decoration: BoxDecoration(color: Colors.orange[100],
+                          decoration: BoxDecoration(
+                            color: Colors.orange[100],
                             borderRadius: BorderRadius.circular(20),
                             boxShadow: shadowList,
                           ),
-                          margin: EdgeInsets.only(top: 40,bottom: 10),
+                          margin: EdgeInsets.only(top: 40, bottom: 10),
                         ),
-                         Align(
-                           child: Image.asset('images/petcat1.png'),
-                         )
+                        Align(
+                          child: Image.asset('images/petcat2.png'),
+                        )
                       ],
                     ),
                   ),
-                  Expanded(child: Container(
-                    margin: EdgeInsets.only(top: 60,bottom: 20),
-                    decoration: BoxDecoration(color: Colors.white,
-                    boxShadow: shadowList,
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(20),
-                      bottomRight: Radius.circular(20),
-                    )),
-
+                  Expanded(
+                      child: Container(
+                    margin: EdgeInsets.only(top: 60, bottom: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: shadowList,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
+                        )),
                   ))
                 ],
               ),
-          )
-
-        ],
+            ),
+            SizedBox(height: 50,)
+          ],
+        ),
       ),
     );
   }
---
+++ pet_home/lib/pages/detailScreen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:pet_home/configuration.dart';
+
+class detailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+              child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.amberAccent.shade100,
+                ),
+              ),
+              Expanded(
+                  child: Container(
+                color: Colors.white,
+              )),
+            ],
+          )),
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Hero(
+                tag: 1,
+                child: Image.asset(
+                  'images/petcat1.png',
+                  height: 400,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 50),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back_ios),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.ios_share_outlined),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: 120,
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: shadowList,
+                  borderRadius: BorderRadius.circular(20)),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              height: 150,
+              decoration: BoxDecoration(
+                  color: Colors.amber.shade100,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30))),
+              child: Row(
+                children: [
+                  SizedBox(width: 10,),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.amberAccent.shade400,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Icon(
+                      Icons.favorite_border,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(width: 10,),
+                  Expanded(
+                      child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.amberAccent.shade400,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text('Adopt Me',
+                          style: TextStyle(color: Colors.white, fontSize: 24)),
+                    ),
+                  )),
+                  SizedBox(width: 10,),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
